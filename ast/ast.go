623859a36c4d843a6d1b4fcd3e6a898d33d8613b
xiang <1335289286@qq.com>
2024-08-13 15:04:10 +0800

    `chore: Add support for parsing expression statements in parser`

--- ast/ast.go
+++ ast/ast.go
@@ -53,3 +53,11 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      token.Token // 该表达式中的第一个词法单元
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
